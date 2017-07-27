ae2380ba4ea5655ed6a6fa19706dbae5d4a134b1
nikhil-1337 <nsavaliya17@gmail.com>
2017-07-27 22:07:14 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,3 @@
+<?php
+    $db = pg_connect("host=ec2-54-221-221-153.compute-1.amazonaws.com dbname=de2fc7r4pt13n1 user=lthwqfdnbwcqvs password=ec3c382e3ec579974a5cbf4de9be4ba386ec84fe69483980b5f555e6b909b48a");
+?>
