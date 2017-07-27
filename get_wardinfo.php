205f9f87a2ca71fc4dd078b18429ea0862720dfa
nikhil-1337 <nsavaliya17@gmail.com>
2017-07-27 22:39:25 +0530

    Update get_wardinfo.php

--- get_wardinfo.php
+++ get_wardinfo.php
@@ -4,19 +4,19 @@ function getWardInfo(){
 	//global $restaurant_id;
 	require 'config.php';
 
-	$print="";
+	$wardinfo="";
 
 	$Query="SELECT * FROM wardinfo WHERE wardno=?";
 	$Result=pg_query($con,$Query);
 	if(isset($Result) && !empty($Result) && pg_num_rows($Result) > 0){
 	$row=pg_fetch_assoc($Result);
 
-	$print=echo "id: " . $row["sqno"]. " - Name: " . $row["name"]. " - Address: " . $row["address"]. " -MobileNo: " . $row["contact"]"<br>";
+	$wardinfo=echo "id: " . $row["sqno"]. " - Name: " . $row["name"]. " - Address: " . $row["address"]. " -MobileNo: " . $row["contact"]"<br>";
         
 		$arr=array(
 			"source" => "RMC",
-			"speech" => $print,
-			"displayText" => $print,
+			"speech" => $wardinfo,
+			"displayText" => $wardinfo,
 		);
 		sendMessage($arr);
 	}else{
