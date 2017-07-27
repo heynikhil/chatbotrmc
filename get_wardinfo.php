7b35666a057d2e91fd2e773806e7c182b0bdd684
nikhil-1337 <nsavaliya17@gmail.com>
2017-07-27 23:55:12 +0530

    Update get_wardinfo.php

--- get_wardinfo.php
+++ get_wardinfo.php
@@ -1,12 +1,12 @@
 <?php
 
-function getWardInfo(){
+function getWardInfo($param){
 	//global $restaurant_id;
 	require 'config.php';
 
 	$wardinfo="";
 
-	$Query="SELECT * FROM wardinfo WHERE wardno=?";
+	$Query="SELECT * FROM wardinfo WHERE wardno=$param";
 	$Result=pg_query($con,$Query);
 	if(isset($Result) && !empty($Result) && pg_num_rows($Result) > 0){
 	$row=pg_fetch_assoc($Result);
