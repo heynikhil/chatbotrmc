bf81548eb21cd17ef6326d2adbba610797087943
Nikhil Savaliya <nsavaliya17@gmail.com>
2017-07-28 00:40:46 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 
 <?php
-$servername = "host=ec2-54-221-221-153.compute-1.amazonaws.com";
+$servername = "ec2-54-221-221-153.compute-1.amazonaws.com";
 $username = "lthwqfdnbwcqvs";
 $password = "ec3c382e3ec579974a5cbf4de9be4ba386ec84fe69483980b5f555e6b909b48a";
 $dbname = "de2fc7r4pt13n1";
