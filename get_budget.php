4768a24e7a488b3c0ef1e32c69af2a4c62c0708d
Nikhil Savaliya <nsavaliya17@gmail.com>
2017-07-29 01:07:14 +0530

    Create get_budget.php

---
+++ get_budget.php
@@ -0,0 +1,31 @@
+<?php
+
+function getBudgetInfo($param){
+	//global $restaurant_id;
+	require 'config.php';
+
+	$getBudgetInfo="";
+
+	$Query="SELECT link FROM public.budget WHERE year=$param";
+	$Result=pg_query($con,$Query);
+	if(isset($Result) && !empty($Result) && pg_num_rows($Result) > 0){
+	$row=pg_fetch_assoc($Result);
+	$getBudgetInfo= " - Link: " . $row["link"];
+
+		$arr=array(
+			"source" => "RMC",
+			"speech" => $getBudgetInfo,
+			"displayText" => $getBudgetInfo,
+		);
+		sendMessage($arr);
+	}else{
+		$arr=array(
+			"source" => "RMC",
+			"speech" => "No year matched in database.",
+			"displayText" => "No year matched in database.",
+		);
+		sendMessage($arr);
+    }
+}
+
+?>
