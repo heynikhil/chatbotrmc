1113313eab7f69db692bdda119df4278731478a0
Nikhil Savaliya <nsavaliya17@gmail.com>
2017-07-29 13:17:45 +0530

    Update get_wardinfo.php

--- get_wardinfo.php
+++ get_wardinfo.php
@@ -11,7 +11,7 @@ function getWardInfo($param){
 	if(isset($Result) && !empty($Result) && pg_num_rows($Result) > 0){
 	$row=pg_fetch_assoc($Result);
 
-	$wardinfo= " - Name: " . $row["name"]. " - Address: " . $row["address"]. " - MobileNo: " . $row["contact"];
+	$wardinfo= "  Name: " . $row["name"]. " --- Address: " . $row["address"]. " --- MobileNo: " . $row["contact"];
         
 		$arr=array(
 			"source" => "RMC",
